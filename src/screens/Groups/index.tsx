a6f316541d055f14ecb87641ca417fbb961a20f0
Daniel Gabriel <reelpscanal@gmail.com>
2023-02-10 16:09:30 -0300

    save backup flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react"
+import { FlatList } from "react-native"
+
+import { GroupCard } from "@components/GroupCard"
+import { Header } from "@components/Header"
+import { Highlight } from "@components/Highlight"
 import { Container } from "./styles"
 
 export function Groups() {
-  return <Container></Container>
+  const [groups, setGroups] = useState([
+    "Galera do Basquete",
+    "Amigos",
+    "Turma do LOL",
+  ])
+
+  return (
+    <Container>
+      <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
+    </Container>
+  )
 }
